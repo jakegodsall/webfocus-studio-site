77b033f11ab7df57cc0d9c5649db821d43cea147
jakegodsall <jake.edward.godsall@gmail.com>
2024-02-12 17:25:51 +0000

    added text centring

--- src/app/(marketing)/components/ComingSoon.jsx
+++ src/app/(marketing)/components/ComingSoon.jsx
@@ -1,8 +1,8 @@
 export default function ComingSoon() {
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-[10rem]">Coming Soon</h1>
-      <p className="text-[1.5rem] w-[40%] text-center">
+    <div className="flex flex-col items-center mx-[5rem] text-center">
+      <h1 className="text-[10rem] leading-[8rem] mb-[3rem]">Coming Soon</h1>
+      <p className="text-[1.5rem]">
         WebFocus Studio is the latest entrant on the digital scene, offering
         dynamic and innovative web design and development services tailored for
         businesses seeking to carve out their unique identity online.
